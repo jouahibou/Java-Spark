5059f064857c409385b3a66f49f12525dc9e838b
jouahibou Diame <81402648+jouahibou@users.noreply.github.com>
2023-04-15 00:04:13 +0200

    Update predictions.java
    Train

--- predictions.java
+++ predictions.java
@@ -11,6 +11,9 @@ import scala.Tuple2;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.mllib.evaluation.MulticlassMetrics;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
 
 
 public class Test {
@@ -20,52 +23,43 @@ public class Test {
    .appName("Data")
    .getOrCreate();
 
-   Dataset<Row> df = spark.read().option("header","true").csv("penguins_size.csv");
+   Dataset<Row> df_raw = spark.read().option("header","true").csv("penguins_size.csv");
    
    StringIndexer indexer = new StringIndexer()
+  .setInputCols(new String[] {"species", "island", "sex","culmen_length_mm","culmen_depth_mm","flipper_length_mm","body_mass_g"})
+  .setOutputCols(new String[] {"label", "islandIndex", "sexIndex","culmen_length_mmIndex","culmen_depth_mmIndex","flipper_length_mmIndex","body_mass_gIndex"});
 
-   .setInputCols(new String[] {"species", "island", "sex","culmen_length_mm","culmen_depth_mm","flipper_length_mm","body_mass_g"})
-   .setOutputCols(new String[] {"speciesIndex", "islandIndex", "sexIndex","culmen_length_mmIndex","culmen_depth_mmIndex","flipper_length_mmIndex","body_mass_gIndex"});
-   Dataset<Row> indexed = indexer.fit(df).transform(df);
-   //indexed.setHandleInvalid("keep");
-   //indexed.show();
-   //indexed.printSchema();
-   //indexed.describe().show();
+Dataset<Row> indexed = indexer.fit(df_raw).transform(df_raw);
 
-   VectorAssembler assembler = new VectorAssembler()
-  .setInputCols(new String[] {"culmen_length_mmIndex", "culmen_depth_mmIndex","flipper_length_mmIndex", "body_mass_gIndex"})
-  .setOutputCol("features");
-Dataset<Row> assembled = assembler.transform(indexed);
-//assembled.select("features").show();
+VectorAssembler assembler = new VectorAssembler()
+  .setInputCols(new String[] {"islandIndex", "culmen_length_mmIndex", "culmen_depth_mmIndex", "flipper_length_mmIndex", "body_mass_gIndex", "sexIndex"})
+  .setOutputCol("features_pre");
+Dataset<Row> data = assembler.transform(indexed).select("label", "features_pre");
+data.show();
 
 StandardScaler scaler = new StandardScaler()
-  .setInputCol("features")
-  .setOutputCol("scaledFeatures")
-  .setWithStd(true)
-  .setWithMean(false);
-StandardScalerModel scalerModel = scaler.fit(assembled);
-Dataset<Row> scaled = scalerModel.transform(assembled);
-//scaled.select("scaledFeatures").show();
-//scaled.describe().show();
-
-VectorAssembler assembler_fin = new VectorAssembler()
-  .setInputCols(new String[] {"scaledFeatures", "islandIndex", "sexIndex"})
-  .setOutputCol("big_features");
-Dataset<Row> data = assembler_fin.transform(scaled).select("speciesIndex", "big_features");
-data = data.withColumnRenamed("speciesIndex", "label");
-data = data.withColumnRenamed("big_features", "features");
-// data.show();
-   
+      .setInputCol("features_pre")
+      .setOutputCol("features")
+      .setWithStd(true)
+      .setWithMean(false);
 LogisticRegression lr = new LogisticRegression();
-LogisticRegressionModel lrModel = lr.fit(data);
 
-JavaRDD<Row> data_rdd = data.toJavaRDD();
-JavaPairRDD< Object, Object> predictionAndLabels = data_rdd.mapToPair(p ->
-  new Tuple2<>(lrModel.predict(p.getAs(1)), p.getAs(0)));
+Pipeline pipeline = new Pipeline().setStages(new PipelineStage[] {scaler, lr});
+
+Dataset<Row>[] data_split = data.randomSplit(new double[] {0.8, 0.2}, 12345);
+Dataset<Row> train = data_split[0];
+Dataset<Row> test = data_split[1];
+
+PipelineModel model = pipeline.fit(train);
+
+Dataset<Row> predictions = model.transform(test);
+
+JavaRDD<Row> predictions_rdd = predictions.toJavaRDD();
+JavaPairRDD< Object, Object> predictionAndLabels = predictions_rdd.mapToPair(p ->
+  new Tuple2<>(p.getAs(5), p.getAs(0)));
 MulticlassMetrics metrics = new MulticlassMetrics(predictionAndLabels.rdd());
 System.out.format("Weighted precision = %f\n", metrics.weightedPrecision());
-
-
+   
 
     }
 }
